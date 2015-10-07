bd73aa1fcfda2a8dc9f33641136e0d5a5c02cd69
Noah Golmant <noah.golmant@gmail.com>
2015-10-07 15:25:12 -0700

    Added article text extraction/filtering using nltk and newspaper modules

--- article.py
+++ article.py
@@ -1,9 +1,17 @@
 from datetime import datetime
-from time import mktime
+from time import mktime, strptime
 import feedparser
 import re
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+import newspaper
+import db_manager
+from article import *
+import argparse
 
 ARTICLE_ENTRY_LENGTH = 4
+stop_words = stopwords.words('english')
+stemmer = SnowballStemmer('english')
 
 class ArticleFormatException(Exception):
     pass
@@ -14,10 +22,10 @@ class Article:
         if len(raw_data) != ARTICLE_ENTRY_LENGTH:
             raise ArticleFormatException("Wrong number of entries for article. Expected {0} but got {1}".format(ARTICLE_ENTRY_LENGTH, len(raw_data)))
 
-        self.__title        = self.__strip_tags(raw_data[0])
-        self.__description  = self.__strip_tags(raw_data[1])
-        self.__link         = raw_data[2]
-        self.__published    = self.__iso_formatted(raw_data[3])
+        self.title        = self.__strip_tags(raw_data[0])
+        self.description  = self.__strip_tags(raw_data[1])
+        self.link         = raw_data[2]
+        self.published    = self.__iso_formatted(raw_data[3])
 
     @classmethod
     def from_feedparser(cls, item):
@@ -28,21 +36,13 @@ class Article:
         except AttributeError:
             return None
             # TODO: crawl for URL if not available
-    @property
-    def title(self):
-        return self.__title
 
-    @property
-    def description(self):
-        return self.__description
-
-    @property
-    def link(self):
-        return self.__link
-
-    @property
-    def published(self):
-        return self.__published
+    @classmethod
+    def from_sqlentry(cls, entry):
+        """Gets article object from an SQL row entry"""
+        published = strptime(entry[-1], '%Y-%m-%d %H:%M:%S')
+        
+        return cls((entry[1],entry[2],entry[3],published))
 
     def sql_entry(self):
         return (self.title, self.description, self.link, self.published)
@@ -53,3 +53,34 @@ class Article:
     # Get ISO-formatted date string for db entry
     def __iso_formatted(self, date):
         return datetime.fromtimestamp(mktime(date))
+    
+    def get_article_text(self):
+        """Uses newspaper module to get main body of an article"""
+        news_article = newspaper.Article(self.link)
+        news_article.download()
+        news_article.parse()
+        self.text = [w.lower() for w in news_article.text.split()]
+        return self.text
+
+    def filter_article_text(self):
+        """Filters out stop words and stems each word to get usable text tokens"""
+        if not hasattr(self, 'text'):
+            self.get_article_text()
+        
+        self.filtered_text = [stemmer.stem(w) for w in self.text if w not in stop_words]
+        return self.filtered_text
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Article function tester')
+    parser.add_argument("-f", "--filter", action="store_true", help="Test text filter on a sample article")
+    parser.add_argument("-p", "--parse", action="store_true", help="Test text parser on a sample article")
+
+    args = parser.parse_args()
+
+    query = "SELECT * FROM articles LIMIT 1;"
+    ret = db_manager.query(query)
+    test_article = Article.from_sqlentry(ret[0])
+    if args.filter:
+        print(test_article.filter_article_text())
+    elif args.parse:
+        print(test_article.get_article_text())
--- db_manager.py
+++ db_manager.py
@@ -1,6 +1,7 @@
 import sqlite3
 from urllib.parse import urlparse
 from contextlib import contextmanager
+from article import *
 
 articles_db = sqlite3.connect('articles.db')
 cursor = articles_db.cursor()
@@ -9,6 +10,7 @@ table_name = 'articles'
 article_rows = [ 'title', 'description', 'link', 'published' ]    
 
 def query(sql, args=None):
+    """Executes an SQLite query on the article database"""
     if args:
         cursor.execute(sql,args)
     else:
@@ -16,20 +18,48 @@ def query(sql, args=None):
     articles_db.commit()
     return cursor.fetchall()
 
+def query_gen(sql, args=None, block_size=20):
+    """Creates a generator for a specific query that
+       yields BLOCK_SIZE database entries per iteration.
+    """
+    def query_gen_helper():
+        if args:
+            cursor.execute(sql, args)
+        else:
+            cursor.execute(sql)
+        articles_db.commit()
+        while True:
+            results = cursor.fetchmany(block_size)
+            if not results:
+                break
+            yield results
+    return query_gen_helper
+
 def num_articles():
+    """Get the number of articles in the database"""
     sql = "SELECT COUNT(*) FROM {}".format(table_name)
     ret = query(sql)
     return ret[0][0]
 
 def get_url_domain(url):
+    """Parse the URL domain of a specified link"""
     parsed = urlparse(url)
     return '{uri.scheme}://{uri.netloc}/'.format(uri=parsed)
 
 def get_unique_publishers():
+    """Get the set of domain names for all articles in the DB"""
     sql = "SELECT link FROM {}".format(table_name)
     ret = query(sql)
     links = [get_url_domain(r[0]) for r in ret]
     return set(links)
+
+def get_articles(funct, block_size=20):
+    """Iterate over blocks of BLOCK_SIZE articles from the database"""
+    sql = "SELECT * FROM {}".format(table_name)
+    article_gen = query_gen(sql,block_size=20)    
+    for article_block in article_gen():
+        a = Article.from_sqlentry(article_block[0])
+        funct(a)
 
 def add_entry(article):
     """ Adds an article entry with the table data:
